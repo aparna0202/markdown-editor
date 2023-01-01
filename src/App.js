54a54ac0670b7c16df4fdf5871eda3f12ac471b3
aparna0202 <aparna.pandey172004@gmail.com>
2023-01-02 00:22:50 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import ReactMarkdown from "react-markdown";
+// import ReactMarkdown from "react-markdown";
 import Editor from "./Components/Editor/Editor";
 
 function App() {
   return (
     <div className="App">
       <Editor />
-      <ReactMarkdown children="**Hello**" />
+      {/* <ReactMarkdown children="**Hello**" /> */}
     </div>
   );
 }
